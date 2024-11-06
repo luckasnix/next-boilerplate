2f09a9dce2b421157d696826db80635ec980007a
Kasnix <luc.kasnix@gmail.com>
2024-11-06 15:59:48 -0300

    chore: upgrade MUI packages

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from 'next';
 import type { ReactNode } from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v14-appRouter';
+import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Roboto } from 'next/font/google';
